1a6363f17e7ff8aa8da5d4937e6cea3a0253ae75
felixzengerle-cpu <felix.zengerle@googlemail.com>
2025-10-22 15:37:29 +0100

    Test

--- Testing_Connection_GIT.py
+++ Testing_Connection_GIT.py
@@ -1,4 +1,18 @@
 #test input
 
-print('hello world')
-print('test')
+import pandas as pd
+
+# Load the Excel file
+file_path = "songs_input.xlsx"
+songs_df = pd.read_excel(file_path)
+
+print(songs_df.columns)
+
+
+# Filter rows where track_id == 7
+track_7 = songs_df[songs_df["track_id"] == 7]
+
+# Show the result
+print(track_7)
+
+
